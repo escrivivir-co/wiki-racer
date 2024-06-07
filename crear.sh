6196c945b29d156e18e98c53a6f2968994b27a77
j <j@j.j>
2024-06-07 17:30:27 +0200

    Script para inicializar el proyecto

---
+++ crear.sh
@@ -0,0 +1,6 @@
+echo "# wiki-racer" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/escrivivir-co/wiki-racer.git
